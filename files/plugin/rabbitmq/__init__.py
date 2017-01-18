f8b96d4e3d63755cb8e29416b74cf2ab70e850b0
Pavel Pulec <kayn@inuits.eu>
2017-01-18 08:55:24 +0100

    switch to another rabbitmq plugin refs #26576
    src: https://github.com/NYTimes/collectd-rabbitmq
    Signed-off-by: Pavel Pulec <kayn@inuits.eu>

---
+++ files/plugin/rabbitmq/__init__.py
@@ -0,0 +1,2 @@
+# This file is managed by puppet
+__version__ = '1.16.0'
--- manifests/plugin/rabbitmq.pp
+++ manifests/plugin/rabbitmq.pp
@@ -4,48 +4,41 @@ class collectd::plugin::rabbitmq (
   $collectd_rabbitmq_password = hiera('collectd_rabbitmq_password', ''),
 ) {
 
-  if $::operatingsystemmajrelease == '6' {
-    $_collect_perl_package = 'collectd-perl'
-  } else {
-    $_collect_perl_package = 'perl-Collectd'
-  }
-
-  package { $_collect_perl_package:
-    ensure => 'present',
-  }
-
-  file { '/usr/local/collectd-plugins/Collectd/':
+  file { '/usr/local/collectd-plugins/rabbitmq':
     ensure  => 'directory',
     group   => '0',
     mode    => '0755',
     owner   => '0',
     require => File['/usr/local/collectd-plugins/'],
-  }
-  file { '/usr/local/collectd-plugins/Collectd/Plugins/':
-    ensure  => 'directory',
-    group   => '0',
-    mode    => '0755',
-    owner   => '0',
-    require => File['/usr/local/collectd-plugins/Collectd'],
-  }
-  file { '/usr/local/collectd-plugins/Collectd/Plugins/RabbitMQ.pm':
-    source  => 'puppet:///modules/collectd/RabbitMQ.pm',
-    mode    => '0644',
-    require => File['/usr/local/collectd-plugins/Collectd/Plugins'],
-  }
-
-  file_line { 'cpusummaryline':
-    path    => '/usr/share/collectd/types.db',
-    line    => 'cpusummary              user:COUNTER:U:U, nice:COUNTER:U:U, system:COUNTER:U:U, idle:COUNTER:U:U, iowait:COUNTER:U:U, irq:COUNTER:U:U, softirq:COUNTER:U:U, cpucount:GAUGE:U:U',
-    match   => '^cpusummary\s+',
-    require => Package['collectd'],
     notify  => Service['collectd'],
   }
-
-  file_line { 'rabbitmqline':
-    path    => '/usr/share/collectd/types.db',
-    line    => 'rabbitmq                messages:GAUGE:0:U, messages_rate:GAUGE:0:U, messages_unacknolwedged:GAUGE:0:U, messages_unacknowledged_rate:GAUGE:0:U, messages_ready:GAUGE:0:U, message_ready_rate:GAUGE:0:U, memory:GAUGE:0:U, consumers:GAUGE:0:U, publish:GAUGE:0:U, publish_rate:GAUGE:0:U, deliver_no_ack:GAUGE:0:U, deliver_no_ack_rate:GAUGE:0:U, deliver_get:GAUGE:0:U, deliver_get_rate:GAUGE:0:U',
-    match   => '^rabbitmq\s+',
+  file { '/usr/local/collectd-plugins/rabbitmq/__init__.py':
+    source  => 'puppet:///modules/collectd/plugin/rabbitmq/__init__.py',
+    mode    => '0644',
+    require => File['/usr/local/collectd-plugins/rabbitmq'],
+    notify  => Service['collectd'],
+  }
+  file { '/usr/local/collectd-plugins/rabbitmq/collectd_plugin.py':
+    source  => 'puppet:///modules/collectd/plugin/rabbitmq/collectd_plugin.py',
+    mode    => '0644',
+    require => File['/usr/local/collectd-plugins/rabbitmq'],
+    notify  => Service['collectd'],
+  }
+  file { '/usr/local/collectd-plugins/rabbitmq/rabbit.py':
+    source  => 'puppet:///modules/collectd/plugin/rabbitmq/rabbit.py',
+    mode    => '0644',
+    require => File['/usr/local/collectd-plugins/rabbitmq'],
+    notify  => Service['collectd'],
+  }
+  file { '/usr/local/collectd-plugins/rabbitmq/utils.py':
+    source  => 'puppet:///modules/collectd/plugin/rabbitmq/utils.py',
+    mode    => '0644',
+    require => File['/usr/local/collectd-plugins/rabbitmq'],
+    notify  => Service['collectd'],
+  }
+  file { '/usr/share/collectd/types.db.rabbitmq':
+    source  => 'puppet:///modules/collectd/plugin/rabbitmq/types.db.rabbitmq',
+    mode    => '0644',
     require => Package['collectd'],
     notify  => Service['collectd'],
   }
