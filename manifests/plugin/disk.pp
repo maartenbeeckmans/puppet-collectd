4af19dfbcc789aacd001301864e638e3e9208cbb
Maarten Beeckmans <maarten.beeckmans@student.hogent.be>
2020-04-03 14:05:29 +0200

    Install package collectd-disk when using el8

--- manifests/plugin/disk.pp
+++ manifests/plugin/disk.pp
@@ -1,6 +1,12 @@
 #
 class collectd::plugin::disk{
 
+  if $facts['os']['family'] == 'RedHat' and $facts['os']['release']['major'] == '8' {
+    package {'collectd-disk':
+      ensure => present,
+    } 
+  }
+
 
   file { '/etc/collectd.d/disk.conf':
     source => 'puppet:///modules/collectd/plugin/disk.conf',
